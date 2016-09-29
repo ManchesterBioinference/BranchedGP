d8d32c8b82e8bc8cb7831cafa622c29c95ae8032
alexisboukouvalas <alexis.boukouvalas@gmail.com>
2016-09-29 17:02:48 +0100

    cleaning up Guo code

--- BayesianOptimiser.py
+++ BayesianOptimiser.py
@@ -1,12 +1,9 @@
 import numpy as np
-import GPyOpt
-import VBHelperFunctions
 
 
 # Objective function
 class objectiveBAndK:
-
-    def __init__(self, mV):
+    def __init__(self, mV, fOptimizeLocal):
         '''
         Pass in initial value for branch point, VB models, Y data, pseudotime t
         and OMGP model
@@ -15,21 +12,23 @@ class objectiveBAndK:
         # Store initial values
         self.likvar = self.mV.likelihood.variance.value  # just fixed nugget
         # Global optimiser: branching location, kernel len, kernel variance - fixed = True
-        assert self.mV.kern.branchkernelparam.kern.variance.fixed is True
+        if(fOptimizeLocal):
+            assert self.mV.kern.branchkernelparam.kern.variance.fixed is True
         # Local optimiser: Phi is reset  in UpdateBranchingModel
         assert mV.likelihood.variance.fixed is False
         assert self.mV.logPhi.fixed is False
         self.lenInitial = self.mV.kern.branchkernelparam.kern.lengthscales.value
+        self.fOptimizeLocal = fOptimizeLocal
 
     def f(self, theta):
         # theta is nxp array, return nx1
         n = theta.shape[0]
         VBboundarray = np.ones((n, 1))
-        if(self.mV.kern.branchkernelparam.kern.lengthscales.fixed):
+        if(self.fOptimizeLocal):
             # BO length scale
             assert theta.shape[1] == 3
         else:
-            assert theta.shape[1] == 2  # just branch point and kern var
+            assert theta.shape[1] == 4  # just branch point, kern var, len, lik var
 
         for i in range(n):
             # Set initial values for local parameters so each run starts the same
@@ -37,18 +36,17 @@ class objectiveBAndK:
             # Set BO'd parameters
             self.mV.UpdateBranchingPoint(np.ones((1, 1)) * theta[i, 0])  # this update phi to initial
             self.mV.kern.branchkernelparam.kern.variance = theta[i, 1]
-            if(self.mV.kern.branchkernelparam.kern.lengthscales.fixed):
-                self.mV.kern.branchkernelparam.kern.lengthscales = theta[i, 2]
+            self.mV.kern.branchkernelparam.kern.lengthscales = theta[i, 2]
+            if not self.fOptimizeLocal:
+                self.mV.likelihood.variance = theta[i, 3]
             else:
-                self.mV.kern.branchkernelparam.kern.lengthscales = self.lenInitial
-            # and optimize local parameters
-            self.mV.optimize()
+                self.mV.optimize()
             VBboundarray[i] = self.mV.objectiveFun()  # we wish to minimize!
-            if(self.mV.kern.branchkernelparam.kern.lengthscales.fixed):
+            if(self.fOptimizeLocal):
                 print('objectiveB B=%.2f kervar=%.2f len=%.2f VB=%.3f' %
                       (theta[i, 0], theta[i, 1], theta[i, 2], VBboundarray[i]))
             else:
-                print('objectiveB B=%.2f kervar=%.2f  VB=%.3f' %
-                      (theta[i, 0], theta[i, 1], VBboundarray[i]))
+                print('objectiveB B=%.2f kervar=%.2f len=%.2f likvar=%.2f VB=%.3f' %
+                      (theta[i, 0], theta[i, 1], theta[i, 2], theta[i, 3], VBboundarray[i]))
 
         return VBboundarray
--- testGuoPlotSingleFit.py
+++ testGuoPlotSingleFit.py
@@ -17,6 +17,45 @@ import BranchingTree as bt
 import branch_kernParamGPflow as bk
 import assigngp_dense
 import BayesianOptimiser
+import matplotlib.cm as cm
+
+
+def plotVBCode(mV, lw=3., fs=10, fPlotVar=False):
+    B = mV.kern.branchkernelparam.Bv.value.flatten()
+    assert B.size == 1, 'Code limited to one branch point, got ' + str(B.shape)
+    pt = mV.t
+    l = np.min(pt)
+    u = np.max(pt)
+    d = 0  # constraint code to be 1D for now
+    for f in range(1, 4):
+        if(f == 1):
+            ttest = np.linspace(l, B, 100)[:, None]  # root
+        else:
+            ttest = np.linspace(B, u, 100)[:, None]
+        Xtest = np.hstack((ttest, ttest * 0 + f))
+        mu, var = mV.predict_f(Xtest)
+        assert np.all(np.isfinite(mu)), 'All elements should be finite but are ' + str(mu)
+        assert np.all(np.isfinite(var)), 'All elements should be finite but are ' + str(var)
+        mean, = plt.plot(ttest, mu[:, d], linewidth=lw)
+        col = mean.get_color()
+        if(fPlotVar):
+            plt.plot(ttest.flatten(), mu[:, d] + 2 * np.sqrt(var.flatten()), '--', color=col, linewidth=lw)
+            plt.plot(ttest, mu[:, d] - 2 * np.sqrt(var.flatten()), '--', color=col, linewidth=lw)
+
+    v = plt.axis()
+    plt.plot([B, B], v[-2:], '-m', linewidth=lw)
+
+
+def plotGene(t,g):
+    with plt.style.context('seaborn-whitegrid'):
+        colors = cm.spectral(np.linspace(0, 1, len(labelLegend)))
+        plt.figure(figsize=(10, 10))
+        for lab, c in zip(labelLegend, colors):
+            y1 = t[labels == lab]
+            y2 = g[labels == lab]
+            plt.scatter(y1,y2,label=lab, c=c,s=80)
+            plt.text(np.median(y1),np.median(y2),lab, fontsize=45, color='blue')
+        plt.legend(loc='upper left')
 
 
 def InitParams(m):
@@ -126,28 +165,21 @@ else:
     bounds = [(t.min(), t.max()), (eps, 5 * Y.var()), (1, 10.*np.ptp(t)), (eps, 1 * Y.var())]
     # Branching point, kernel var, kernel len, lik vari
 
-print('Initialised mb', mb)
-mb.UpdateBranchingPoint(np.ones((1, 1))*0.2)
-myobj = BayesianOptimiser.objectiveBAndK(mb, fOptimizeLocal)
-print('Bounds used in optimisation: =', bounds)
-t0 = time.time()
-BOobj = GPyOpt.methods.BayesianOptimization(f=myobj.f, bounds=bounds)
-max_iter = 100
-nrestart = 1
-n_cores = 2
-BOobj.run_optimization(max_iter,                            # Number of iterations
-                       acqu_optimize_method='fast_random',  # method to optimize the acq. function
-                       acqu_optimize_restarts=nrestart,
-                       batch_method='lp',
-                       n_inbatch=n_cores,                   # size of the collected batches (= number of cores)
-                       eps=1e-6)                            # secondary stop criteria (on top of iters)
-print('Bayesian optimisation took %g secs. ' % (time.time() - t0))
-print('Solution found by BO x_opt =  ' + str(BOobj.x_opt) + 'fx_opt = ' + str(BOobj.fx_opt))
-if(fPlot):
-    objAtMin = BOobj.f(BOobj.x_opt[None, :])  # get solution, update mb
-    VBHelperFunctions.plotVBCode(mb, labels=labels, figsizeIn=(5, 5), fPlotVar=True)
-    plt.title('%s B=%g ll=%.2f' % (ginter, mb.kern.branchkernelparam.Bv.value, objAtMin))
-    plt.savefig("figs/GuoBestfit_%s.png" % ginter, bbox_inches='tight')
-    pickle.dump(mb, open("figs/GuoBestfit_%s.p" % ginter, "wb"))
-    #  read with pickle.load(open( "~/Dropbox/BranchedGP/figs/GuoBestfit_%s.p" % ginter, "rb" ))
+parametersTry = [0.35, 2.41, 1.04]  # Fgf4
 
+# parametersTry = [0.16, 4.85, 1.06]  # Pdgfra
+mb.UpdateBranchingPoint(np.ones((1, 1))*parametersTry[0])
+Kbranch.branchkernelparam.kern.variance = np.array(parametersTry[1])
+Kbranch.branchkernelparam.kern.lengthscales = np.array(parametersTry[2])
+print('Initialised mb', mb)
+VBHelperFunctions.plotVBCode(mb, labels=labels, figsizeIn=(5, 5), fPlotVar=True)
+plt.title('Init %s B=%g ll=%.2f' % (ginter, mb.kern.branchkernelparam.Bv.value, mb.objectiveFun()))
+
+plotGene(t, Y)
+plotVBCode(mb, lw=3., fs=10, fPlotVar=True)
+plt.title('%s B=%g' % (ginter, mb.kern.branchkernelparam.Bv.value))
+
+# 
+# mb.optimize()
+# VBHelperFunctions.plotVBCode(mb, labels=labels, figsizeIn=(5, 5), fPlotVar=True)
+# plt.title('Optimised %s B=%g ll=%.2f' % (ginter, mb.kern.branchkernelparam.Bv.value, mb.objectiveFun()))
