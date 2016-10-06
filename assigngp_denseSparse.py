e4496d3136201b801c14c87ce47ac01912161794
James Hensman <james.hensman@gmail.com>
2016-10-06 16:40:19 +0200

    some simplifications in sparse assignGP

--- assigngp_denseSparse.py
+++ assigngp_denseSparse.py
@@ -3,9 +3,9 @@
 import GPflow
 import numpy as np
 import tensorflow as tf
-import pZ_construction_singleBP
-from matplotlib import pyplot as plt
-from GPflow.param import AutoFlow
+# import pZ_construction_singleBP
+# from matplotlib import pyplot as plt
+# from GPflow.param import AutoFlow
 import assigngp_dense
 
 
@@ -32,10 +32,8 @@ class AssignGPSparse(assigngp_dense.AssignGP):
 
     def __init__(self, t, XExpanded, Y, kern, indices, phiInitial, b, ZExpanded, fDebug=False):
         assigngp_dense.AssignGP.__init__(self, t, XExpanded, Y, kern, indices, phiInitial, b, fDebug)
-#         self.ZExpanded = GPflow.param.Param(ZExpanded)  # inducing points for sparse GP, optional. Same format as XExpanded
-       # Do not treat inducing points as parameters because they should always be fixed. 
-       # because we should not change index as parameter.. right?
-        self.ZExpanded = GPflow.param.DataHolder(ZExpanded)  # inducing points for sparse GP, optional. Same format as XExpanded
+        # Do not treat inducing points as parameters because they should always be fixed.
+        self.ZExpanded = GPflow.param.DataHolder(ZExpanded)  # inducing points for sparse GP. Same as XExpanded
         assert ZExpanded.shape[1] == XExpanded.shape[1]
 
     def build_likelihood(self):
@@ -47,34 +45,27 @@ class AssignGPSparse(assigngp_dense.AssignGP):
         # try squashing Phi to avoid numerical errors
         Phi = (1-2e-6) * Phi + 1e-6
 
-        tau = 1./self.likelihood.variance
+        sigma2 = self.likelihood.variance
+        sigma = tf.sqrt(self.likelihood.variance)
         Kuu = self.kern.K(self.ZExpanded) + GPflow.tf_hacks.eye(M) * 1e-6
         Kuf = self.kern.K(self.ZExpanded, self.X)
-        if(self.fDebug):
-            Kuu = tf.Print(Kuu, [tf.shape(Kuu), Kuu], message='Kuu=', name='Kuu', summarize=10)
-            Kuf = tf.Print(Kuf, [tf.shape(Kuf), Kuf], message='Kuf=', name='Kuf', summarize=10)
 
         Kdiag = self.kern.Kdiag(self.X)
         L = tf.cholesky(Kuu)
-        W = tf.matrix_triangular_solve(L, Kuf)
         p = tf.reduce_sum(Phi, 0)
-        LTA = W * tf.sqrt(p)
-        P = tf.matmul(LTA, tf.transpose(LTA)) * tau + GPflow.tf_hacks.eye(M)
-        traceTerm = -0.5 * tau * (tf.reduce_sum(Kdiag*p) - tf.reduce_sum(tf.square(LTA)))
+        LiKuf = tf.matrix_triangular_solve(L, Kuf)
+        W = LiKuf * tf.sqrt(p) / sigma
+        P = tf.matmul(W, tf.transpose(W)) + GPflow.tf_hacks.eye(M)
+        traceTerm = -0.5 * tf.reduce_sum(Kdiag * p) / sigma2 + 0.5 * tf.reduce_sum(tf.square(W))
         R = tf.cholesky(P)
-        PhiY = tf.matmul(Kuf, tf.matmul(tf.transpose(Phi), self.Y))
-        # LPhiY = tf.matmul(tf.transpose(L), PhiY)
-        LiPhiY = tf.matrix_triangular_solve(L, PhiY)
-        RiLPhiY = tf.matrix_triangular_solve(R, LiPhiY, lower=True)
-        KL = self.build_KL(Phi)
-        if(self.fDebug):
-            traceTerm = tf.Print(traceTerm, [tf.shape(KL), KL], message='KL=', name='KL', summarize=10)
+        tmp = tf.matmul(LiKuf, tf.matmul(tf.transpose(Phi), self.Y))
+        c = tf.matrix_triangular_solve(R, tmp, lower=True) / sigma
 
-        self.bound = traceTerm + 0.5*N*D*tf.log(tau)\
+        self.bound = traceTerm - 0.5*N*D*tf.log(2 * np.pi * sigma2)\
             - 0.5*D*tf.reduce_sum(tf.log(tf.square(tf.diag_part(R))))\
-            - 0.5*tau*tf.reduce_sum(tf.square(self.Y))\
-            + 0.5*tf.reduce_sum(tf.square(tau * RiLPhiY))
-        self.bound = self.bound - KL
+            - 0.5*tf.reduce_sum(tf.square(self.Y)) / sigma2\
+            + 0.5*tf.reduce_sum(tf.square(c))\
+            - self.build_KL(Phi)
 
         return self.bound
 
@@ -85,25 +76,24 @@ class AssignGPSparse(assigngp_dense.AssignGP):
         # try squashing Phi to avoid numerical errors
         Phi = (1-2e-6) * Phi + 1e-6
 
-        sigma = tf.sqrt(self.likelihood.variance)
+        sigma2 = self.likelihood.variance
+        sigma = tf.sqrt(sigma2)
         Kuu = self.kern.K(self.ZExpanded) + GPflow.tf_hacks.eye(M) * 1e-6
         Kuf = self.kern.K(self.ZExpanded, self.X)
         L = tf.cholesky(Kuu)
-        W = tf.matrix_triangular_solve(L, Kuf)
 
         p = tf.reduce_sum(Phi, 0)
-        A = W * tf.sqrt(p) / sigma
-
-        B = tf.matmul(A, tf.transpose(A)) + GPflow.tf_hacks.eye(M)
-        R = tf.cholesky(B)
-
-        AY = tf.matmul(W, tf.matmul(Phi, self.Y)) / sigma
-        c = tf.matrix_triangular_solve(R, AY, lower=True) / sigma
+        LiKuf = tf.matrix_triangular_solve(L, Kuf)
+        W = LiKuf * tf.sqrt(p) / sigma
+        P = tf.matmul(W, tf.transpose(W)) + GPflow.tf_hacks.eye(M)
+        R = tf.cholesky(P)
+        tmp = tf.matmul(LiKuf, tf.matmul(tf.transpose(Phi), self.Y))
+        c = tf.matrix_triangular_solve(R, tmp, lower=True) / sigma
 
         Kus = self.kern.K(self.ZExpanded, Xnew)
         tmp1 = tf.matrix_triangular_solve(L, Kus, lower=True)
         tmp2 = tf.matrix_triangular_solve(R, tmp1, lower=True)
-        mean = tf.matmul(tf.transpose(tmp2), c)
+        mean = tf.matmul(tf.transpose(tmp2), c) / sigma
         if full_cov:
             var = self.kern.K(Xnew) + tf.matmul(tf.transpose(tmp2), tmp2)\
                 - tf.matmul(tf.transpose(tmp1), tmp1)
