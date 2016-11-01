ad0474323e047d1e8ffe842197ac319624f8397d
alexisboukouvalas <alexis.boukouvalas@gmail.com>
2016-11-01 11:28:28 +0000

    cluster job script

--- synthetic/r.sh
+++ synthetic/r.sh
@@ -3,8 +3,11 @@
 #$ -cwd                   # Run job from directory where submitted
 #$ -V                     # Inherit environment (modulefile) settings
 # Load modulefiles....
-module load apps/gcc/tensorflow/0.10.0-py34-cpu
+# Running jobs
 module load tools/env/proxy
-# Run whatever commands you want
-# exec jug execute $1
-python $1
+module load tools/gcc/git/2.8.2
+module load apps/binapps/anaconda/3/4.1.1
+export PATH=/opt/gridware/apps/gcc/tensorflow/fixes:$PATH
+export PYTHONPATH=$HOME/.local/lib/python3.5/site-packages
+# Run 
+LD_PRELOAD="/usr/lib64/librt.so:/opt/gridware/apps/gcc/tensorflow/fixes/stubs/mylibc.so:/opt/gridware/compilers/gcc/4.8.2/lib64/libstdc++.so.6" /opt/gridware/apps/binapps/anaconda/3/2.3.0/bin/python "$@" $1
