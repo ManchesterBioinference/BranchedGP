d17de1118d4aa91447c58c64544305d1c5b81b8f
Elvijs Sarkans <elvijsdr@gmail.com>
2020-09-05 23:13:59 +0100

    Update the setup file to include main dependencies
    Pinned to TF1 and GPflow1. The plan is to stabilise this version
    and then perform an upgrade and relax the requirements.

--- setup.py
+++ setup.py
@@ -12,17 +12,22 @@ LICENSE = 'MIT'
 
 VERSION = '0.1'
 
-setup(name=NAME,
-      version=VERSION,
-      description=DESCRIPTION,
-      long_description=LONG_DESCRIPTION,
-      author=AUTHOR,
-      author_email=AUTHOR_EMAIL,
-      maintainer=MAINTAINER,
-      maintainer_email=MAINTAINER_EMAIL,
-      url=DOWNLOAD_URL,
-      download_url=DOWNLOAD_URL,
-      license=LICENSE,
-      packages=['BranchedGP'],
-      package_data={}
-      )
+setup(
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    maintainer=MAINTAINER,
+    maintainer_email=MAINTAINER_EMAIL,
+    url=DOWNLOAD_URL,
+    download_url=DOWNLOAD_URL,
+    license=LICENSE,
+    packages=['BranchedGP'],
+    package_data={},
+    install_requires=[
+        "tensorflow==1.15.3",
+        "gpflow==1.5.1",
+    ],
+)
